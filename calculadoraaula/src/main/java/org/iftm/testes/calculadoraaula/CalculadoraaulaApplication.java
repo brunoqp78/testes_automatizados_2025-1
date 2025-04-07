e4550cef0ebe77cd8123c37f078d2bafa043f0d8
brunoqp78 <bruno.queiroz@iftm.edu.br>
2025-04-07 15:04:22 -0300

    criando projeto calculadora aula

---
+++ calculadoraaula/src/main/java/org/iftm/testes/calculadoraaula/CalculadoraaulaApplication.java
@@ -0,0 +1,13 @@
+package org.iftm.testes.calculadoraaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraaulaApplication.class, args);
+	}
+
+}
---
+++ calculadoraaula/src/test/java/org/iftm/testes/calculadoraaula/CalculadoraaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iftm.testes.calculadoraaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
